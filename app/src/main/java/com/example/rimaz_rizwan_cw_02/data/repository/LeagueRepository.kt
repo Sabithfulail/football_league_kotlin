81e87cb22e107b4b4ac998a01a6c9c44292df006
Sabithfulail <fulail.20212170@iit.ac.lk>
2024-05-21 16:18:22 +0530

    search clubs by league updated

---
+++ app/src/main/java/com/example/rimaz_rizwan_cw_02/data/repository/LeagueRepository.kt
@@ -0,0 +1,17 @@
+package com.example.rimaz_rizwan_cw_02.data.repository
+
+import com.example.rimaz_rizwan_cw_02.data.entity.Club
+import com.example.rimaz_rizwan_cw_02.data.entity.League
+import kotlinx.coroutines.flow.Flow
+
+interface LeagueRepository {
+    /**
+     * Retrieve all the league from the the given data source.
+     */
+    fun getAllLeagueStream(): Flow<List<League>>
+
+
+    suspend fun insertListOfLeague(league: List<League>)
+    suspend fun insertListOfClub(clubs: List<Club>)
+
+}

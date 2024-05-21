ec047f39c2c2eae15921d0ccb6f8a3bfbfd2cd9a
Sabithfulail <fulail.20212170@iit.ac.lk>
2024-05-21 16:18:38 +0530

    minor changes

--- app/src/main/java/com/example/rimaz_rizwan_cw_02/ui/add_league_to_db/LeaguesInDbScreen.kt
+++ app/src/main/java/com/example/rimaz_rizwan_cw_02/ui/add_league_to_db/LeaguesInDbScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -33,7 +32,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.rimaz_rizwan_cw_02.R
-import com.example.rimaz_rizwan_cw_02.data.League
+import com.example.rimaz_rizwan_cw_02.data.entity.League
 import com.example.rimaz_rizwan_cw_02.ui.AppViewModelProvider
 import com.example.rimaz_rizwan_cw_02.ui.navigation.NavigationDestination
 

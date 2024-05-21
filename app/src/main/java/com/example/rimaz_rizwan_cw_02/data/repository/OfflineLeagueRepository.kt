5b3932bf2dd386ed7034bac9c8835a4d205f897c
Sabithfulail <fulail.20212170@iit.ac.lk>
2024-05-21 16:18:49 +0530

    search clubs by league updated

---
+++ app/src/main/java/com/example/rimaz_rizwan_cw_02/data/repository/OfflineLeagueRepository.kt
@@ -0,0 +1,15 @@
+package com.example.rimaz_rizwan_cw_02.data.repository
+
+import com.example.rimaz_rizwan_cw_02.data.dao.ClubDao
+import com.example.rimaz_rizwan_cw_02.data.entity.League
+import com.example.rimaz_rizwan_cw_02.data.dao.LeagueDao
+import com.example.rimaz_rizwan_cw_02.data.entity.Club
+import kotlinx.coroutines.flow.Flow
+
+class OfflineLeagueRepository(private val itemDao: LeagueDao, private val clubDao: ClubDao) : LeagueRepository {
+    override fun getAllLeagueStream(): Flow<List<League>> = itemDao.getAllItems()
+
+    override suspend fun insertListOfLeague(league: List<League>) = itemDao.insertListOfLeagues(league)
+    override suspend fun insertListOfClub(clubs: List<Club>) = clubDao.insertListOfClub(clubs)
+
+}

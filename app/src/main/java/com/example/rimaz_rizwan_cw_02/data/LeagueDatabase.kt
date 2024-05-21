a4830f0f7a8f5d2bf8aff5d5f582938d91e392a6
Sabithfulail <fulail.20212170@iit.ac.lk>
2024-05-21 16:18:13 +0530

    search clubs by league updated

--- app/src/main/java/com/example/rimaz_rizwan_cw_02/data/LeagueDatabase.kt
+++ app/src/main/java/com/example/rimaz_rizwan_cw_02/data/LeagueDatabase.kt
@@ -4,11 +4,16 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.rimaz_rizwan_cw_02.data.dao.ClubDao
+import com.example.rimaz_rizwan_cw_02.data.dao.LeagueDao
+import com.example.rimaz_rizwan_cw_02.data.entity.Club
+import com.example.rimaz_rizwan_cw_02.data.entity.League
 
-@Database(entities = [League::class], version = 1, exportSchema = false)
+@Database(entities = [League::class, Club::class], version = 1, exportSchema = false)
 abstract class LeagueDatabase : RoomDatabase() {
 
     abstract fun leagueDao(): LeagueDao
+    abstract fun clubDao(): ClubDao
 
     companion object {
         @Volatile

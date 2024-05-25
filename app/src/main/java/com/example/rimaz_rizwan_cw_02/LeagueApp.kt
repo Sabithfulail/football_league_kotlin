b62d55da6872bf398891cced4e4b99d5308ad3cd
Sabithfulail <fulail.20212170@iit.ac.lk>
2024-05-25 11:02:35 +0530

    minor changes

--- app/src/main/java/com/example/rimaz_rizwan_cw_02/LeagueApp.kt
+++ app/src/main/java/com/example/rimaz_rizwan_cw_02/LeagueApp.kt
@@ -2,7 +2,6 @@ package com.example.rimaz_rizwan_cw_02
 
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
